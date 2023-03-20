da0919953d2728e636867ab500fb7049526a4e68
pgp30 <paulpagsuyuin@gmail.com>
2023-03-20 16:36:04 +0800

    Initial commit

---
+++ Application/app/src/main/java/com/pagsuyuin/application_employment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pagsuyuin.application_employment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
